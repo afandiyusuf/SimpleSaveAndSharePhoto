714201e0ad70941574718431dae74adeb4758608
yusuf <afandi.yusuf.04@gmail.com>
2016-04-02 03:58:33 +0700

    take picture and share complete

---
+++ Assets/AndroidUltimatePluginLite/Scripts/Main/Utils.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System;
+
+namespace AUP{
+	public class Utils{
+		public static void Message(string tag, string message){
+			Debug.LogWarning(tag + message);
+		}
+		
+		//take screen shot then share via intent
+		public static IEnumerator TakeScreenshot(string screenShotPath,string screenShotName){
+			yield return new WaitForEndOfFrame();
+			
+			int width = Screen.width;
+			int height = Screen.height;
+			Texture2D tex = new Texture2D(width, height, TextureFormat.RGB24, false);
+			
+			// Read screen contents into the texture
+			tex.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+			
+			tex.Apply();
+
+			byte[] screenshot;
+
+			string[] pathParts = screenShotName.Split('.');
+			
+			if(pathParts.Length > 1){
+				string format = pathParts.GetValue(1).ToString();
+				Debug.Log( " format " + format );
+				if(format.Equals("png",StringComparison.Ordinal)){
+					screenshot = tex.EncodeToPNG();
+					Debug.Log( "screen shot set to png format");
+				}else if(format.Equals("jpg",StringComparison.Ordinal)){
+					screenshot = tex.EncodeToJPG();
+					Debug.Log( "screen shot set to jpg format");
+				}else{
+					screenshot = tex.EncodeToJPG();
+					Debug.Log( "screen shot unknown format default to jpg");
+				}
+			}else{
+				screenshot = tex.EncodeToJPG();
+				Debug.Log( "screen shot no format default to jpg");
+			}
+
+			//saving to phone storage
+			System.IO.File.WriteAllBytes(screenShotPath,screenshot);
+		}
+	}
+}
---
+++ Assets/PreviewAndSave.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+using System.Collections;
+using Vuforia;
+using UnityEngine.UI;
+public class PreviewAndSave : MonoBehaviour {
+
+	Texture2D tex;
+	public VuforiaBehaviour camera;
+	public GameObject takeButton;
+	public GameObject saveButton;
+	public GameObject shareButton;
+	public GameObject retakeButton;
+	CaptureAndSave snapShot ;
+	public RawImage previewer;
+	public GameObject canvas;
+	public ShareAndExperienceDemo SAE;
+	public AndroidNativeUIDemo AND;
+	public bool isSave = false;
+	public string path;
+
+
+	void Start()
+	{
+		snapShot = GameObject.FindObjectOfType<CaptureAndSave>();
+		takeButton.SetActive(true);
+		retakeButton.SetActive(false);
+		shareButton.SetActive(false);
+		saveButton.SetActive(false);
+	}
+
+	void OnEnable()
+	{
+		CaptureAndSaveEventListener.onError += OnError;
+		CaptureAndSaveEventListener.onSuccess += OnSuccess;
+		CaptureAndSaveEventListener.onScreenShotInvoker += OnScreenShot;
+	}
+
+	void OnDisable()
+	{
+		CaptureAndSaveEventListener.onError -= OnError;
+		CaptureAndSaveEventListener.onSuccess -= OnSuccess;
+		CaptureAndSaveEventListener.onScreenShotInvoker -= OnScreenShot;
+	}
+
+	void OnError(string error)
+	{
+		Debug.Log ("Error : "+error);
+		AND.ShowAlertPopup(error);
+	}
+
+	void OnSuccess(string msg)
+	{
+		path = msg;
+		AND.ShowToastMessage("Image berhasil di save di "+msg);
+		AND.ShowToastMessage("Saving image ");
+		saveButton.SetActive(false);
+		shareButton.SetActive(true);
+	}
+
+	void OnScreenShot(Texture2D tex2D)
+	{
+		AND.ShowToastMessage("Take image success");
+		camera.enabled = false;
+		previewer.gameObject.SetActive(true);
+		tex = tex2D;
+		previewer.texture = tex;
+		canvas.SetActive(true);
+	}
+	public void TakeCamera()
+	{
+		canvas.SetActive(false);
+		previewer.gameObject.SetActive(true);
+		snapShot.GetFullScreenShot();
+
+		takeButton.SetActive(false);
+		retakeButton.SetActive(true);
+		saveButton.SetActive(true);
+		isSave = false;
+	}
+
+	public void SaveScreenShotToGallery()
+	{
+		snapShot.SaveTextureToGallery(tex);
+
+
+	}
+	public void EnableButton()
+	{
+
+		isSave = true;
+	}
+	public void Share()
+	{
+		SAE.ShareImageS(path);
+	}
+
+	public void RetakeCamera()
+	{
+		previewer.gameObject.SetActive(false);
+		shareButton.SetActive(false);
+		takeButton.SetActive(true);
+		retakeButton.SetActive(false);
+		saveButton.SetActive(false);
+		camera.enabled = true;
+	}
+}
